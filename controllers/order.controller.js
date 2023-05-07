72bad0457d923d848399fee56850a2218e5aa1c9
IMRANDIL <aliimranadil2@gmail.com>
2023-05-07 22:43:09 +0530

    added

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -11,7 +11,18 @@ exports.createOrder = async (req, res) => {
     await order.save();
 
     // Send a message to the Order Processor
-    const worker = new RSMQWorker("orderQueue");
+    const worker = new RSMQWorker({
+      host: process.env.REDIS_HOST, // Redis host address (Docker container IP)
+      port: process.env.REDIS_PORT, // Redis port (default: 6379)
+      // ns: process.env.REDIS_NAMESPACE, // Optional: Redis namespace
+      // Other optional configuration options can be added here
+      queueName: "orderQueue", // Name of the RSMQ queue
+    });
+
+    // Start the RSMQ worker
+    worker.start();
+
+    // Send the order as a message
     worker.send(JSON.stringify(order), (err, messageId) => {
       if (err) {
         console.error("Failed to send message to Order Processor:", err);
